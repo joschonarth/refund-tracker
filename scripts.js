ff0a2fef38239cb8ad87264af44fa3bbbb2f5db9
joschonarth <joschonarth@gmail.com>
2024-11-03 10:47:34 -0300

    Update Refund

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
+amount.oninput = () => {
+    let value = amount.value.replace(/\D/g, "");
+    
+    value = Number(value) / 100
+
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        "currency": "BRL",
+    })    
+    return value
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+}
